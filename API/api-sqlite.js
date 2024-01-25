7a0b259a360a76345272172d155f0bb0c12c69d7
Robin <thatrobin3001@gmail.com>
2024-01-25 00:31:58 +0000

    changed api-sqlite,js

--- API/api-sqlite.js
+++ API/api-sqlite.js
@@ -8,7 +8,7 @@ db.serialize(function() {
 var express = require('express');
 var app = express();
 
-app.use(express.json());
+app.use(express.json());eggewegw2e
 
 /**
  * @api {get} /mods/ Get all mods
